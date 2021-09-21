6b2e43d84d997180258638e6cdfdd89f2539911a
Manuel Mauky <manuel.mauky@zeiss.com>
2021-09-21 18:04:52 +0200

    teil 1, listing 2

--- src/profile-info.js
+++ src/profile-info.js
@@ -8,6 +8,41 @@ class ProfileInfo extends HTMLElement {
                 <img id="profile-image">
             </div>
         `
+        this.update()
+    }
+
+    static get observedAttributes() {
+        return ["username", "image-url"]
+    }
+
+    get userName() {
+        return this.getAttribute("username")
+    }
+    set userName(newValue) {
+        this.setAttribute("username", newValue)
+    }
+
+    get imageUrl() {
+        return this.getAttribute("image-url")
+    }
+    set imageUrl(newValue) {
+        this.setAttribute("image-url", newValue)
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.update()
+    }
+
+    update() {
+        const userNameElement = this.querySelector("#user-name")
+        if(userNameElement) {
+            userNameElement.textContent = this.userName
+        }
+
+        const profileImageElement = this.querySelector("#profile-image")
+        if(profileImageElement) {
+            profileImageElement.src = this.imageUrl
+        }
     }
 }
 
